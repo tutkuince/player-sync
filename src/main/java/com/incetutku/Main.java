e704b6f4f561225e4ab9d0adf5f82e96307fd258
Tutku Ince <tutku.ince@outlook.com>
2024-11-02 18:30:58 +0300

    Main App

---
+++ src/main/java/com/incetutku/Main.java
@@ -0,0 +1,29 @@
+package com.incetutku;
+
+public class Main {
+    private static final int MAX_MESSAGE_COUNT = 10;
+
+    public static void main(String[] args) throws InterruptedException {
+        Player initiator = new Player("Initiator");
+        Player responder = new Player("Responder");
+
+
+        Thread sendMessage = new Thread(() -> {
+            for (int i = 0; i < MAX_MESSAGE_COUNT; i++) {
+                initiator.sendMessage(initiator);
+            }
+        });
+
+        Thread receiveMessage = new Thread(() -> {
+            for (int i = 0; i < MAX_MESSAGE_COUNT; i++) {
+                responder.receiveMessage();
+            }
+        });
+
+        sendMessage.start();
+        receiveMessage.start();
+
+        sendMessage.join();
+        receiveMessage.join();
+    }
+}
