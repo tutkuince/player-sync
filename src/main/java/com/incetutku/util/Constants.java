299e5861b195077db868526a5e72d1516c8af3df
Tutku Ince <tutku.ince@outlook.com>
2024-11-03 21:38:44 +0300

    Constants

---
+++ src/main/java/com/incetutku/util/Constants.java
@@ -0,0 +1,11 @@
+package com.incetutku.util;
+
+public class Constants {
+    public static final int MAX_MESSAGE_COUNT = 10;
+
+    private Constants() {
+        throw new AssertionError();
+    }
+
+
+}
