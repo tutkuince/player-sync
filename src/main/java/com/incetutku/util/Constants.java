cb13e5d71c8b6deee7f57a8e4ac89200ba5e9171
Tutku Ince <tutku.ince@outlook.com>
2024-11-04 18:42:40 +0300

    Constants

--- src/main/java/com/incetutku/util/Constants.java
+++ src/main/java/com/incetutku/util/Constants.java
@@ -2,6 +2,9 @@ package com.incetutku.util;
 
 public class Constants {
     public static final int MAX_MESSAGE_COUNT = 10;
+    public static final int PORT = 12345; // The server's port
+    public static final String SERVER_ADDRESS = "localhost"; // The server's address
+
 
     private Constants() {
         throw new AssertionError();
