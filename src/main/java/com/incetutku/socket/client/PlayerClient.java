fdf86d8f64f5a0540d5a9a90c94eba8913627647
Tutku Ince <tutku.ince@outlook.com>
2024-11-07 18:25:10 +0300

    updated

--- src/main/java/com/incetutku/socket/client/PlayerClient.java
+++ src/main/java/com/incetutku/socket/client/PlayerClient.java
@@ -1,9 +1,11 @@
 package com.incetutku.socket.client;
 
 import com.incetutku.entity.Message;
-import com.incetutku.socket.AbstractPlayer;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
 import static com.incetutku.util.Constants.*;
 
@@ -12,38 +14,100 @@ import static com.incetutku.util.Constants.*;
  * It connects to a specified server (Initiator) on a defined host and port,
  * continuously receives messages, and sends back responses.
  */
-public class PlayerClient extends AbstractPlayer {
+public class PlayerClient {
+    private final String name;
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private int counter;
 
     /**
      * Constructs a PlayerClient instance with the name "Responder".
      */
     public PlayerClient() {
-        super("Responder");
+        this.name = "Responder";
+        this.counter = 0;
     }
 
     /**
      * Starts the PlayerClient by establishing a connection to the server, then continuously
      * receiving messages and responding back until 10 messages have been sent.
      *
-     * @throws IOException if an I/O error occurs while establishing or using the connection
+     * @throws IOException            if an I/O error occurs while establishing or using the connection
      * @throws ClassNotFoundException if a received message cannot be deserialized
      */
-    @Override
     public void start() throws IOException, ClassNotFoundException {
         try {
-            // Initialize the connection to the server
             initializeConnection(SERVER_ADDRESS, PORT);
 
-            // Receive and respond to messages until the counter reaches 10
             while (counter < MAX_MESSAGE_COUNT) {
                 Message receivedMessage = receiveMessage();
 
-                // Send a response back to the Initiator
                 sendMessage("Hello back " + counter);
             }
         } finally {
-            // Ensure that the connection is closed after communication is complete
             closeConnection();
         }
+    }
+
+    /**
+     * Initializes a connection to the specified host and port.
+     * Sets up the socket, output stream, and input stream for communication.
+     *
+     * @param host the host to connect to
+     * @param port the port number to connect to
+     * @throws IOException if an I/O error occurs while creating the socket or streams
+     */
+    private void initializeConnection(String host, int port) throws IOException {
+        socket = new Socket(host, port);
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+        System.out.printf("%s connected to %s:%s\n", name, host, port);
+    }
+
+    /**
+     * Closes the socket connection and associated streams.
+     * This method should be called when the player has completed its communication.
+     * If any of the resources fail to close, a descriptive error message is printed.
+     */
+    private void closeConnection() {
+        try {
+            if (socket != null) socket.close();
+            if (out != null) out.close();
+            if (in != null) in.close();
+        } catch (IOException e) {
+            System.err.printf("%s: Failed to close input stream - %s\n", name, e.getMessage());
+        }
+    }
+
+    /**
+     * Sends a message to the connected player with the specified content.
+     * Each message includes the sender's name, content, and a sequence number.
+     *
+     * @param content the content of the message to send
+     * @throws IOException if an I/O error occurs while sending the message
+     */
+    private void sendMessage(String content) throws IOException {
+        Message message = Message.builder()
+                .sender(name)
+                .content(content)
+                .sequence(++counter)
+                .build();
+        System.out.println(message);  // toString() method from Message class will be used
+        out.writeObject(message);
+    }
+
+    /**
+     * Receives a message from the connected player.
+     * The received message is printed to the console.
+     *
+     * @return the received Message object
+     * @throws IOException            if an I/O error occurs while reading the message
+     * @throws ClassNotFoundException if the message cannot be deserialized into a Message object
+     */
+    private Message receiveMessage() throws IOException, ClassNotFoundException {
+        Message message = (Message) in.readObject();
+        System.out.printf("%s received: %s\n", name, message);
+        return message;
     }
 }
--- src/main/java/com/incetutku/socket/server/PlayerServer.java
+++ src/main/java/com/incetutku/socket/server/PlayerServer.java
@@ -1,12 +1,12 @@
 package com.incetutku.socket.server;
 
 import com.incetutku.entity.Message;
-import com.incetutku.socket.AbstractPlayer;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 import static com.incetutku.util.Constants.*;
 
@@ -16,13 +16,19 @@ import static com.incetutku.util.Constants.*;
  * Once a connection is established, it starts the communication by sending the first message
  * and then alternates between receiving and sending messages.
  */
-public class PlayerServer extends AbstractPlayer {
+public class PlayerServer {
+    private final String name;
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private int counter;
 
     /**
      * Constructs a PlayerServer instance with the name "Initiator".
      */
     public PlayerServer() {
-        super("Initiator");
+        this.name = "Initiator";
+        this.counter = 0;
     }
 
     /**
@@ -32,30 +38,85 @@ public class PlayerServer extends AbstractPlayer {
      * @throws IOException            if an I/O error occurs while establishing or using the connection
      * @throws ClassNotFoundException if a received message cannot be deserialized
      */
-    @Override
     public void start() throws IOException, ClassNotFoundException {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.printf("%s started on port %s\n", name, PORT);
 
-            // Accepts a connection from the client (Responder)
             socket = serverSocket.accept();
             out = new ObjectOutputStream(socket.getOutputStream());
             in = new ObjectInputStream(socket.getInputStream());
             System.out.printf("Client connected to %s\n", name);
 
-            // Send the first message to initiate communication
             sendMessage("Hello");
 
-            // Receive and respond to messages until the counter reaches 10
             while (counter < MAX_MESSAGE_COUNT) {
                 Message receivedMessage = receiveMessage();
 
-                // Send a response message with the updated counter value
                 sendMessage("Hello " + counter);
             }
         } finally {
-            // Ensures the connection is closed after communication completes
             closeConnection();
         }
+    }
+
+    /**
+     * Initializes a connection to the specified host and port.
+     * Sets up the socket, output stream, and input stream for communication.
+     *
+     * @param host the host to connect to
+     * @param port the port number to connect to
+     * @throws IOException if an I/O error occurs while creating the socket or streams
+     */
+    private void initializeConnection(String host, int port) throws IOException {
+        socket = new Socket(host, port);
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+        System.out.printf("%s connected to %s:%s\n", name, host, port);
+    }
+
+    /**
+     * Closes the socket connection and associated streams.
+     * This method should be called when the player has completed its communication.
+     * If any of the resources fail to close, a descriptive error message is printed.
+     */
+    private void closeConnection() {
+        try {
+            if (socket != null) socket.close();
+            if (out != null) out.close();
+            if (in != null) in.close();
+        } catch (IOException e) {
+            System.err.printf("%s: Failed to close input stream - %s\n", name, e.getMessage());
+        }
+    }
+
+    /**
+     * Sends a message to the connected player with the specified content.
+     * Each message includes the sender's name, content, and a sequence number.
+     *
+     * @param content the content of the message to send
+     * @throws IOException if an I/O error occurs while sending the message
+     */
+    private void sendMessage(String content) throws IOException {
+        Message message = Message.builder()
+                .sender(name)
+                .content(content)
+                .sequence(++counter)
+                .build();
+        System.out.println(message);  // toString() method from Message class will be used
+        out.writeObject(message);
+    }
+
+    /**
+     * Receives a message from the connected player.
+     * The received message is printed to the console.
+     *
+     * @return the received Message object
+     * @throws IOException            if an I/O error occurs while reading the message
+     * @throws ClassNotFoundException if the message cannot be deserialized into a Message object
+     */
+    private Message receiveMessage() throws IOException, ClassNotFoundException {
+        Message message = (Message) in.readObject();
+        System.out.printf("%s received: %s\n", name, message);
+        return message;
     }
 }
