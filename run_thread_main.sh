0925d005a1d249c946c9425ba4f3e3755d41966d
Tutku Ince <tutku.ince@outlook.com>
2025-04-19 19:19:52 +0300

    deleted

--- run_thread_main.sh
+++
@@ -1,9 +0,0 @@
-# Package the project as a JAR (only needs to be done once or after code changes)
-mvn clean package -P thread-main
-
-# Run the JAR file
-java -jar target/thread-main.jar
-
-# Optional: Wait for the server process to finish
-echo "Press any key to end the program..."
-read -n 1 -s  # Waits for a single key press
