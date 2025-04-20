6b66b391e2a6f54f31404a03e7a345534cf4c67d
Tutku Ince <tutku.ince@outlook.com>
2025-04-20 17:55:22 +0300

    update

--- src/main/java/com/incetutku/player/sync/MainApp.java
+++ src/main/java/com/incetutku/player/sync/MainApp.java
@@ -1,10 +1,15 @@
 package com.incetutku.player.sync;
 
 import com.incetutku.player.sync.common.BaseApp;
+import com.incetutku.player.sync.socket.ClientApp;
+import com.incetutku.player.sync.socket.SocketServer;
 import com.incetutku.player.sync.thread.ThreadMain;
 
 public class MainApp {
     public static void main(String[] args) {
-        BaseApp app = new ThreadMain();
+//        BaseApp app = new ThreadMain();
+
+        BaseApp app = new SocketServer();
+        BaseApp client = new ClientApp();
     }
 }
