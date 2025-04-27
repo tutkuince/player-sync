ca2689f5541d2c744c3fdfc18c66de1a7cab8b17
Tutku Ince <tutku.ince@outlook.com>
2025-04-27 18:54:54 +0300

    MainApp has been updated

--- src/main/java/com/incetutku/player/sync/MainApp.java
+++ src/main/java/com/incetutku/player/sync/MainApp.java
@@ -7,31 +7,12 @@ import com.incetutku.player.sync.thread.ThreadMain;
 
 public class MainApp {
     public static void main(String[] args) {
-//        BaseApp app = new ThreadMain();
-
-//        BaseApp app = new SocketServer();
-//        BaseApp client = new ClientApp();
-
-        //        if ("--thread".equals(args[0])){
-//           BaseApp app = new ThreadMain();
-//
-//        }
-//        else if ("--client".equals(args[0])){
-//           BaseApp app = new ClientApp();
-//
-//        }
-//       else if ("--server".equals(args[0])){
-//          BaseApp   app = new SocketServer();
-//
-//        }
-
-
-        if (args[0].equals("--server")) {
-            new SocketServer();
-        } else {
-            new ClientApp();
+        if ("--thread".equals(args[0])) {
+            BaseApp app = new ThreadMain();
+        } else if ("--client".equals(args[0])) {
+            BaseApp app = new ClientApp();
+        } else if ("--server".equals(args[0])) {
+            BaseApp app = new SocketServer();
         }
-
-
     }
 }
